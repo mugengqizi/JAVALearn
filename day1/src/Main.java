3cec963b62ebd775ce5f256af50d30743c504614
GaoMing <2949516603@qq.com>
2024-11-20 10:51:47 +0800

    12111

--- day1/src/Main.java
+++ day1/src/Main.java
@@ -3,7 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("输入一个整数");
+        int i1= sc.nextInt();
+        sc.nextLine();
+        //如果不加这一行会直接跳过s1的输入
+        System.out.println("输入一个字符串");
         String s1=sc.nextLine();
+        System.out.println("输入一个字符串");
+        String s2=sc.nextLine();
+
     }
 
 }
