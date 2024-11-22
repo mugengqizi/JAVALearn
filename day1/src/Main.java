654a11ce0e1d8f693f97bffc857a003202abbec0
GaoMing <2949516603@qq.com>
2024-11-22 17:25:17 +0800

    12111

--- day1/src/Main.java
+++ day1/src/Main.java
@@ -82,8 +82,9 @@ class MultiplicationTable{
     public static void main(String[] args) {
         for (int i = 1; i <=9; i++) {
             for (int j = 1; j <=i ; j++) {
-                System.out.print(j+"*"+i+"="+i*j+" ");
+                System.out.print(j+"*"+i+"="+i*j+" \t");
             }
+            // \t可以对齐
             System.out.println();
         }
     }
